418b9626a0681d348f234890d738cdf599158c6f
Michael Bandola <24bandola@gmail.com>
2017-06-08 20:51:58 -0400

    scene2

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,15 +1,29 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
 
+	public GameObject applePrefab;
+	public float speed=1f;
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirections=0.1f;
+	public float secondsBetweenAppleDrops=1f;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed);
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed);
+		}
 	}
 }
